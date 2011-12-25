931fa4de86ad8029514889f83dc527bfab2d395a
Glen Husman <wappokickingdeer@gmail.com>
2011-12-25 14:10:05 -0800

    Fixed some lint-found warnings.

--- gen/org/glen_h/games/wildwool/R.java
+++ gen/org/glen_h/games/wildwool/R.java
@@ -52,37 +52,35 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040001;
-        public static final int bt_not_enabled_leaving=0x7f040004;
-        public static final int button_scan=0x7f04000e;
-        /**  Options Menu 
-         */
-        public static final int connect=0x7f04000f;
-        public static final int discoverable=0x7f040010;
-        public static final int grow=0x7f040013;
-        public static final int message=0x7f040000;
-        public static final int none_found=0x7f04000b;
-        public static final int none_paired=0x7f04000a;
-        public static final int not_connected=0x7f040003;
-        public static final int p2=0x7f040018;
-        public static final int p3=0x7f040019;
-        public static final int p4=0x7f04001a;
-        public static final int roll=0x7f040012;
-        public static final int roll_die=0x7f040011;
-        /**   DeviceListActivity 
-         */
-        public static final int scanning=0x7f040008;
-        public static final int select_device=0x7f040009;
         /**   BluetoothChat 
          */
-        public static final int send=0x7f040002;
-        public static final int shear=0x7f040015;
-        public static final int swap=0x7f040017;
-        public static final int title_connected_to=0x7f040006;
-        public static final int title_connecting=0x7f040005;
-        public static final int title_not_connected=0x7f040007;
-        public static final int title_other_devices=0x7f04000d;
-        public static final int title_paired_devices=0x7f04000c;
-        public static final int winner_unknown=0x7f040014;
-        public static final int wolf=0x7f040016;
+        public static final int bt_not_enabled_leaving=0x7f040002;
+        public static final int button_scan=0x7f04000c;
+        /**  Options Menu 
+         */
+        public static final int connect=0x7f04000d;
+        public static final int discoverable=0x7f04000e;
+        public static final int grow=0x7f040011;
+        public static final int message=0x7f040000;
+        public static final int none_found=0x7f040009;
+        public static final int none_paired=0x7f040008;
+        public static final int p2=0x7f040016;
+        public static final int p3=0x7f040017;
+        public static final int p4=0x7f040018;
+        public static final int roll=0x7f040010;
+        public static final int roll_die=0x7f04000f;
+        /**   DeviceListActivity 
+         */
+        public static final int scanning=0x7f040006;
+        public static final int select_device=0x7f040007;
+        public static final int shear=0x7f040013;
+        public static final int swap=0x7f040015;
+        public static final int title_connected_to=0x7f040004;
+        public static final int title_connecting=0x7f040003;
+        public static final int title_not_connected=0x7f040005;
+        public static final int title_other_devices=0x7f04000b;
+        public static final int title_paired_devices=0x7f04000a;
+        public static final int winner_unknown=0x7f040012;
+        public static final int wolf=0x7f040014;
     }
 }
