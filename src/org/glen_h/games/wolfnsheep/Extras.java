a389b40a7b4fabb7990eb9e5f3db30808b9cc472
Glen Husman <wappokickingdeer@gmail.com>
2012-02-05 08:39:58 -0800

    Added (didn't implement) "Critical alerts" extra.

--- src/org/glen_h/games/wolfnsheep/Extras.java
+++ src/org/glen_h/games/wolfnsheep/Extras.java
@@ -18,21 +18,26 @@ public class Extras extends ListActivity {
 	protected static final String PREFS_NAME = "extras";
     String current_state_shearcosts;
     String current_state_autoshear;
+    String current_state_criticalalerts;
 	AlertDialog.Builder autoshear;
 	AlertDialog.Builder shearcosts;
+	SharedPreferences settings;
+	AlertDialog.Builder criticalalerts;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
-		final String[] items = new String[]{"Auto-shear","Shearing costs"};
+		final String[] items = new String[]{"Auto-shear","Shearing costs","Critical alerts"};
 		setListAdapter(new ArrayAdapter<String>(this, R.layout.list_item, items));
 		  ListView lv = getListView();
 		  lv.setTextFilterEnabled(true);
-		  SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+		  settings = getSharedPreferences(PREFS_NAME, 0);
 	      boolean autoshear_state = settings.getBoolean("autoshear", true);
 	      boolean shearcosts_state = settings.getBoolean("shearcosts", false);
+	      boolean criticalalerts_state = settings.getBoolean("criticalalerts", true);
 	      final SharedPreferences.Editor editor = settings.edit();
 		  autoshear = new AlertDialog.Builder(this);
+		  criticalalerts = new AlertDialog.Builder(this);
 		  final AlertDialog.Builder restart = new AlertDialog.Builder(this);
 		  restart.setTitle("Restart Required");
 		  restart.setMessage("To apply changes, please restart wolf 'n sheep.");
@@ -57,61 +62,112 @@ public class Extras extends ListActivity {
 		  }else{
 			  current_state_shearcosts = "Disabled";
 		  }
-		  autoshear.setTitle("Auto-shear");
+		  if(criticalalerts_state){
+			  current_state_criticalalerts = "Enabled";
+		  }else{
+			  current_state_criticalalerts = "Disabled";
+		  }
+		  autoshear.setTitle(items[0]);
 		  autoshear.setMessage("Automatically shear when you have 5 wool at the beginning of your turn\nCurrrent Preference: "+current_state_autoshear);
 		  autoshear.setPositiveButton("Enable", new DialogInterface.OnClickListener() {
 				public void onClick(DialogInterface dialog, int whichButton) {
 	            	// Enable extra
+					final boolean old_autoshear = settings.getBoolean("autoshear", true);
 					editor.putBoolean("autoshear", true);
 					editor.commit();
 					current_state_autoshear = "Enabled";
 					autoshear.setMessage("Automatically shear when you have 5 wool at the beginning of your turn\nCurrrent Preference: "+current_state_autoshear);
+					if(old_autoshear != true){
 					restart.show();
+					}
 				}
 			});
 		  autoshear.setNegativeButton("Disable",
 					new DialogInterface.OnClickListener() {
 						public void onClick(DialogInterface dialog, int whichButton) {
 							// Disable extra
+							final boolean old_autoshear = settings.getBoolean("autoshear", true);
 							editor.putBoolean("autoshear", false);
 							editor.commit();
 							current_state_autoshear = "Disabled";
 							autoshear.setMessage("Automatically shear when you have 5 wool at the beginning of your turn\nCurrrent Preference: "+current_state_autoshear);
-							restart.show();
+							if(old_autoshear != false){
+								restart.show();
+							}
+						}
+					});
+		  criticalalerts.setTitle(items[2]);
+		  criticalalerts.setMessage("Alert you when something critical happens to your sheep\nCurrrent Preference: "+current_state_criticalalerts);
+		  // TODO Put buttons here
+		  criticalalerts.setPositiveButton("Enable", new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog, int whichButton) {
+	            	// Enable extra
+					final boolean old_criticalalerts = settings.getBoolean("criticalalerts", true);
+					editor.putBoolean("criticalalerts", true);
+					editor.commit();
+					current_state_criticalalerts = "Enabled";
+					criticalalerts.setMessage("Alert you when something critical happens to your sheep\nCurrrent Preference: "+current_state_criticalalerts);
+					if(old_criticalalerts != true){
+					restart.show();
+					}
+				}
+			});
+		  criticalalerts.setNegativeButton("Disable",
+					new DialogInterface.OnClickListener() {
+						public void onClick(DialogInterface dialog, int whichButton) {
+							// Disable extra
+							final boolean old_criticalalerts = settings.getBoolean("criticalalerts", true);
+							editor.putBoolean("criticalalerts", false);
+							editor.commit();
+							current_state_criticalalerts = "Disabled";
+							criticalalerts.setMessage("Alert you when something critical happens to your sheep\nCurrrent Preference: "+current_state_criticalalerts);
+							if(old_criticalalerts != false){
+								restart.show();
+							}
 						}
 					});
 		  
-		  shearcosts.setTitle("Shearing costs");
+		  shearcosts.setTitle(items[1]);
 		  shearcosts.setMessage("Add a cost of 1 wool to shear\nCurrrent Preference: "+current_state_shearcosts);
 		  shearcosts.setPositiveButton("Enable", new DialogInterface.OnClickListener() {
 				public void onClick(DialogInterface dialog, int whichButton) {
 	            	// Enable extra
+					final boolean old_shearcosts = settings.getBoolean("shearcosts", false);
 					editor.putBoolean("shearcosts", true);
 					editor.commit();
 					current_state_shearcosts = "Enabled";
 					shearcosts.setMessage("Add a cost of 1 wool to shear\nCurrrent Preference: "+current_state_shearcosts);
+					if(old_shearcosts != true){
 					restart.show();
+					}
 				}
 			});
 		  shearcosts.setNegativeButton("Disable",
 					new DialogInterface.OnClickListener() {
 						public void onClick(DialogInterface dialog, int whichButton) {
 							// Disable extra
+							final boolean old_shearcosts = settings.getBoolean("shearcosts", false);
 							editor.putBoolean("shearcosts", false);
 							editor.commit();
 							current_state_shearcosts = "Disabled";
 							shearcosts.setMessage("Add a cost of 1 wool to shear\nCurrrent Preference: "+current_state_shearcosts);
-							restart.show();
+							if(old_shearcosts != false){
+								restart.show();
+							}
 						}
 					});
 		  lv.setOnItemClickListener(new OnItemClickListener() {
 		    public void onItemClick(AdapterView<?> parent, View view,
 		        int position, long id) {
 		      // When clicked, show a toast with the TextView text
-		      if(((TextView) view).getText() == items[0]){
+		      TextView tv = (TextView) view;
+		      if(tv.getText() == items[0]){
 		    	  autoshear.show();
-		  	  }else if(((TextView) view).getText() == items[1]){
+		  	  }else if(tv.getText() == items[1]){
 		  		shearcosts.show();
+		  	  }
+		  	else if(tv.getText() == items[2]){
+		  		criticalalerts.show();
 		  	  }
 		    }
 		  });
