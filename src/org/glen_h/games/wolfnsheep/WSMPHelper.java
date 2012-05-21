b46df67169ae9522163e5f438aed2d0444f4e2a2
Glen Husman <wappokickingdeer@gmail.com>
2012-05-20 20:19:53 -0700

    Added WSMPHelper class.

---
+++ src/org/glen_h/games/wolfnsheep/WSMPHelper.java
@@ -0,0 +1,10 @@
+package org.glen_h.games.wolfnsheep;
+
+public class WSMPHelper {
+
+	private WolfNSheep interfacer = null;
+	
+	protected WSMPHelper(WolfNSheep ws){
+		this.interfacer = ws;
+	}
+}
