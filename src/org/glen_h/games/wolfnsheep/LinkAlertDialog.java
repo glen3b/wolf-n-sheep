7c9214786c6f3ba8a80a0e48b8cec063965fda8c
Glen Husman <wappokickingdeer@gmail.com>
2012-07-02 10:46:08 -0700

    Temporarily made LinkAlertDialog non-linking.

--- src/org/glen_h/games/wolfnsheep/LinkAlertDialog.java
+++ src/org/glen_h/games/wolfnsheep/LinkAlertDialog.java
@@ -2,15 +2,12 @@ package org.glen_h.games.wolfnsheep;
 
 import android.app.AlertDialog;
 import android.content.Context;
-import android.text.SpannableString;
-import android.text.method.LinkMovementMethod;
-import android.text.util.Linkify;
-import android.widget.TextView;
 
 public class LinkAlertDialog{
 
 		public static AlertDialog create(Context context, String title, String message_txt, String dismiss_text) {
-		  final TextView message = new TextView(context);
+		  /*
+			final TextView message = new TextView(context);
 		  // i.e.: R.string.dialog_message =>
 		            // "Test this dialog following the link to dtmilano.blogspot.com"
 		  final SpannableString s = 
@@ -18,13 +15,15 @@ public class LinkAlertDialog{
 		  Linkify.addLinks(s, Linkify.WEB_URLS);
 		  message.setText(s);
 		  message.setMovementMethod(LinkMovementMethod.getInstance());
-
+		  */
+			
 		  return new AlertDialog.Builder(context)
 		   .setTitle(title)
-		   .setCancelable(true)
+		   .setMessage(message_txt)
+		   .setCancelable(false)
 		   // .setIcon(android.R.drawable.ic_dialog_info)
 		   .setPositiveButton(dismiss_text, null)
-		   .setView(message)
+		   // .setView(message)
 		   .create();
 		   /*
 			final TextView message = new TextView(context);
