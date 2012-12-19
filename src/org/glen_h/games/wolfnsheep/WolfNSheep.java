2a2c5b3d0c6047000766002ee6324fd2d24d7df2
Glen Husman <wappokickingdeer@gmail. com>
2012-12-19 10:22:57 -0800

    Removed import lines

--- src/org/glen_h/games/wolfnsheep/WolfNSheep.java
+++ src/org/glen_h/games/wolfnsheep/WolfNSheep.java
@@ -24,7 +24,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
-
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
@@ -33,7 +32,6 @@ import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
-
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
@@ -70,7 +68,6 @@ public class WolfNSheep extends Activity {
 	
 	private ProgressDialog load;
 
-	
 	
 	/**
 	 * Returns the game's vital statistics as a string.
